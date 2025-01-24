extrinsic parama calculation fix

sandbox/extrinsic_params_calculator.py
@@ -3,11 +3,23 @@ import numpy as np
 # Function to compute the rotation matrix and translation vector
 def compute_extrinsics(theta_deg, r):
     theta_rad = np.deg2rad(theta_deg)
-    R = np.array([
-        [np.cos(theta_rad), 0, np.sin(theta_rad)],
+    R_1 = np.array([
+        [np.cos(theta_rad), 0, -np.sin(theta_rad)],
         [0, 1, 0],
-        [-np.sin(theta_rad), 0, np.cos(theta_rad)]
+        [np.sin(theta_rad), 0, np.cos(theta_rad)]
     ])
+    R_z = np.array([
+        [0,1,0],
+        [-1,0,0],
+        [0,0,1]
+    ])
+    R_x = np.array([
+        [1,0,0],
+        [0,0,-1],
+        [0,1,0]
+    ])
+    R_zx = np.dot(R_x,R_z)
+    R = np.dot(R_1,R_zx)
     t = np.array([
         [r * np.cos(theta_rad)],
         [0],